b2b02ef75e79f9d68b920afb7354e40e8d92802d
rrowrrow <rrowiasrur@gmail.com>
2026-09-07 18:46:43 +0800

    Update config.env.js

--- config.env.js
+++ config.env.js
@@ -1,10 +1,10 @@
 // File konfigurasi terpusat
 window.firebaseConfig = {
-  apiKey: "ISI_FIREBASE_API_KEY",
-  authDomain: "ISI_FIREBASE_AUTH_DOMAIN",
-  projectId: "ISI_FIREBASE_PROJECT_ID",
-  storageBucket: "ISI_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "ISI_FIREBASE_MESSAGING_SENDER_ID",
-  appId: "ISI_FIREBASE_APP_ID",
-  measurementId: "ISI_FIREBASE_MEASUREMENT_ID",
+  apiKey: "AIzaSyB-6lfqdfOzqfoDp118bpvi7SmNufTU9Y8",
+  authDomain: "katga-bd583.firebaseapp.com",
+  projectId: "katga-bd583",
+  storageBucket: "katga-bd583.firebasestorage.app",
+  messagingSenderId: "70271543625",
+  appId: "1:70271543625:web:f4fa186d5d98391b95d298",
+  measurementId: "G-QB23BTQYPV",
 };
